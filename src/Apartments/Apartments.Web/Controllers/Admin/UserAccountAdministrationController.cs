c501670941cc0c8f0440991c6f539a48bed5d927
Denis Makarchuk <hiedmant@gmail.com>
2020-03-20 13:02:38 +0300

    [+] methods in IUserAccountAdministrationService.cs & UserAccountAdministrationController.cs

--- src/Apartments/Apartments.Web/Controllers/Admin/UserAccountAdministrationController.cs
+++ src/Apartments/Apartments.Web/Controllers/Admin/UserAccountAdministrationController.cs
@@ -4,12 +4,127 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Apartments.Domain.Logic.Admin.AdminServiceIntersaces;
 
 namespace Apartments.Web.Controllers.Admin
 {
+    /// <summary>
+    /// The controller for the administrator to work with User accounts
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class UserAccountAdministrationController : ControllerBase
     {
+        private readonly IUserAccountAdministrationService _userAccountAdministrationService;
+
+        public UserAccountAdministrationController(IUserAccountAdministrationService userAccountAdministrationService)
+        {
+            _userAccountAdministrationService = userAccountAdministrationService;
+        }
+
+        /// <summary>
+        /// Create new User account
+        /// </summary>
+        /// <param name="subAdminAccount"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("")]
+        public async Task<IActionResult> CreateUserAsync([FromBody] User user)
+        {
+            if (user is null || ModelState.IsValid) // todo: validate User
+            {
+                return BadRequest(ModelState);
+            }
+
+            var result = await _userAccountAdministrationService.CreateUser(user); //will return Result<User>
+
+            return result.IsError ? BadRequest(result.Message) : (IActionResult)Ok(result.Data);
+        }
+
+        /// <summary>
+        /// Get all User accounts
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> GetAllUserAsync()
+        {
+            try
+            {
+                var result = await _userAccountAdministrationService.GetAllUser();
+
+                return result is null ? NotFound() : (IActionResult)Ok(result);
+            }
+            catch (InvalidOperationException ex)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Get User account by Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetUserByIdAsync(string id)
+        {
+            if (string.IsNullOrEmpty(id) || !Guid.TryParse(id, out var _))
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                var result = await _userAccountAdministrationService.GetUserById(id);
+
+                return result is null ? NotFound() : (IActionResult)Ok(result);
+            }
+            catch (InvalidOperationException ex)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Update the User account
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        [HttpPut]
+        [Route("")]
+        public async Task<IActionResult> UpdateUserAsync([FromBody] User user)
+        {
+            if (user is null || ModelState.IsValid) // todo: validate User
+            {
+                return BadRequest(ModelState);
+            }
+
+            var result = await _userAccountAdministrationService.UpdateUser(user); //will return Result<User>
+
+            return result.IsError ? BadRequest(result.Message) : (IActionResult)Ok(result.Data);
+        }
+
+        /// <summary>
+        /// Delete User account by Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> DeleteUserByIdAsync(string id)
+        {
+            if (string.IsNullOrEmpty(id) || !Guid.TryParse(id, out var _))
+            {
+                return BadRequest();
+            }
+
+            var result = await _userAccountAdministrationService.DeleteUser(id); //will return Result
+
+            return result.IsError ? BadRequest(result.Message) : (IActionResult)Ok(result.IsSuccess);
+        }
     }
 }
